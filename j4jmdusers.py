f8557737d480d8b6aa6accd2798d13b35c79807c
lsingerfox <lynell.r.singer@gmail.com>
2021-11-10 13:03:17 -0800

    Added multiple flask packages and updated signup function

--- j4jmdusers.py
+++ j4jmdusers.py
@@ -1,108 +1,128 @@
-from flask import Flask, request, jsonify, Response
+from bson.objectid import ObjectId
+from flask import Flask, request, jsonify, session,redirect,url_for
+from flask.templating import render_template
 from flask_jwt import JWT, jwt_required, current_identity
 from pymongo import MongoClient
-from flask_pymongo import PyMongo
-from werkzeug.security import safe_str_cmp, generate_password_hash
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, PasswordField, BooleanField, IntegerField,SubmitField
+from wtforms.validators import InputRequired, Length
+import certifi
+from werkzeug.security import check_password_hash, safe_str_cmp, generate_password_hash
+from flask_login import login_required, logout_user, current_user
 
 class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
+    def __init__(self, _id, email, password):
+        self._id = _id
+        self.email = email
         self.password = password
-    
 
+    
     def __str__(self):
         return "User(id=%s')" % self.id
 
-
-users = [
-    User(1, 'user1', 'abcxyz'),
-    User(2, 'user2', 'abcxyz'),
-]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
-
-def authentication(username, password):
-    user = username_table.get(username, None)
-    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
-        return user
+    def authentication(email, password):
+        user = db.users.get(email, None)
+        if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+            return user
 
 
-def identity(payload):
-    user_id = payload['identity']
-    return userid_table.get(user_id, None)
+    def identity(payload):
+        _id = payload['identity']
+        return db.users.get(_id, None)
 
+    
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+
+
+csrf = CSRFProtect()
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'H0verm@gic'
-app.config['MONGO_URI'] = "mongodb+srv://lrsinger:Und3rt%40lel0ver2015@dippin-dots-j4j-dont-te.xwqye.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.config['MONGO_URI'] = "mongodb+srv://lrsinger:Und3rt4lel0ver2015@database.xwqye.mongodb.net/database?retryWrites=true&w=majority"
 
-jwt = JWT(app, authentication, identity)
+csrf.init_app(app)
 
-MongoClient = PyMongo(app)
-db = MongoClient.db
+jwt = JWT(app, User.authentication, User.identity)
 
-#Creating New User
-@app.route('/token', methods=['POST'])
-def create_user():
-    firstName = request.json['firstName']
-    lastName = request.json['lastName']
-    companyName = request.json['companyName']
-    username = request.json['username']
-    password = request.json['password']
-    streetAddress = request.json['streetAddress']
-    city = request.json['city']
-    state = request.json['state']
-    zip = request.json['zip']
-    phone = request.json['phone']
-    email = request.json['email']
-    iceCreamPrice = request.json['iceCreamPrice']
-    spoonPrice = request.json['spoonPrice']
-    admin = request.json['admin']
-    active = request.json['active']
+s = MongoClient("mongodb+srv://lrsinger:Und3rt4lel0ver2015@database.xwqye.mongodb.net/database?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+db = s.db
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Length(min=8, max=100)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
+    remember = BooleanField('Remember Me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Length(min=8, max=100)])
+    firstName = StringField('First Name', validators=[InputRequired(), Length(min=2, max=100)])
+    lastName = StringField('Last Name', validators=[InputRequired(), Length(min=2, max=100)])
+    companyName = StringField('Company Name', validators=[Length(min=0, max=100)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
+    streetAddress = StringField('Street Address', validators=[InputRequired(), Length(min=4, max=100)])
+    city = StringField('City', validators=[InputRequired(), Length(min=3, max=60)])
+    state = StringField('State', validators=[InputRequired(), Length(2)])
+    zip = IntegerField('Zip', validators=[InputRequired()])
+    phone = IntegerField('Phone', validators=[InputRequired()])
+    admin = BooleanField(False)
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    form = RegisterForm()
+
+    form.password.data = generate_password_hash(form.password.data)
+
+    if db.users.find_one({ "email": form.email.data }):
+        return jsonify({ "error": "Email address already in use "}), 400
+
+    if form.validate_on_submit() and db.users.insert_one(form):
+        return jsonify({ "msg": "Welcome to the world of Dippin' Dots by Jump 4 Joy"}), 200
+
+    print(form.errors)
+
+    return jsonify({ "error": "Signup failed" }), 400
+
+@app.route('/login/', methods=['POST'])
+def login():
+    form = LoginForm()
+    email= request.json['email']
+    user = db.users.find_one({"email": email})
+    print(form.password.data)
+    print(user['password'])
+    if user and check_password_hash(user['password'],form.password.data):
+        #return render_template('dashboard.html')
+        return jsonify ({ "msg": "Welcome in!" }), 200
+    else:
+        print('Invalid email or password')
+        return jsonify ({ "error" : "Invalid email or password" }), 401
+
     
 
-    if username and email and password:
-        hashed_password = generate_password_hash(password)
-        id = db.users.insert(
-            {'firstName': firstName, 
-            'lastName': lastName, 
-            'companyName': companyName,
-            'username': username, 
-            'password': hashed_password, 
-            'streetAddress': streetAddress, 
-            'city': city, 
-            'state': state, 
-            'zip': zip, 
-            'phone': phone,
-            'email': email,
-            'iceCreamPrice': iceCreamPrice,
-            'spoonPrice': spoonPrice,
-            'admin': admin,
-            'active': active
-            }
-        )
-        response = jsonify({
-            '_id': str(id),
-            'firstName': firstName, 
-            'lastName': lastName, 
-            'companyName': companyName,
-            'username': username,
-            'password': password,
-            'streetAddress': streetAddress, 
-            'city': city, 
-            'state': state, 
-            'zip': zip, 
-            'phone': phone,
-            'email': email,
-            'iceCreamPrice': iceCreamPrice,
-            'spoonPrice': spoonPrice,
-            'admin': admin,
-            'active': active
-        })
-        return response
+
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.firstName)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/protected')

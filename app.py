928a97e16e336c42c5fe5248fdf23ac201548732
lsingerfox <lynell.r.singer@gmail.com>
2021-12-24 12:45:26 -0800

    Moved routes into class UserSession and added def to assist with ServerSelectionTimeoutError

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify,json,session, redirect, render_template, url_for
-from pymongo import MongoClient
+from pymongo import MongoClient, database
 from flask_jwt import jwt_required, current_identity
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
@@ -38,56 +38,81 @@ s = MongoClient ("mongodb+srv://lrsinger:Und3rt4lel0ver2015@database.xwqye.mongo
 db = s.db
 CORS(app)
 
+
+def setUpClass(cls):
+    from pymongo.errors import ServerSelectionTimeoutError
+    from pymongo import MongoClient
+
+    cls.has_mongo_connection = False
+
+    try:
+        client = MongoClient(
+            serverSelectionTimeoutMS=0.1
+        )
+        client.server_info()
+
+        cls.adapter = MongoDatabaseAdapter(
+            database_uri = 'mongodb+srv://lrsinger:Und3rt4lel0ver2015@database.xwqye.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        )
+
+        cls.has_mongo_connection = True
+
+    except ServerSelectionTimeoutError:
+        pass
+
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Length(min=8, max=100)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
 
 
-@app.route("/", methods = ["POST","GET"])
-def home():
-    return "j4j API"
 
-@app.route("/user/login", methods = ["POST"])
-def login():
-    form = LoginForm()
-    email = request.json['email']
-    user = db.admin.find_one({"email": email})
-    print(form.password.data)
-    print(user['password'])  
-    if user and check_password_hash(user['password'], form.password.data):
-        return jsonify({ "msg": "Welcome Back, " + json.loads(json_util.dumps(user["name"])) + "!"}), 200
-    else:
-        return jsonify({ "error": "Invalid email or password"}), 401
+class UserSession():
+    @app.route("/", methods = ["POST","GET"])
+    def home():
+        return "j4j API"
 
-
-@app.route('/dashboard/')
-@login_required
-def dashboard():
-    return render_template('dashboard.html', name=current_user.name)
+    @app.route("/user/login", methods = ["POST"])
+    def login():
+        form = LoginForm()
+        email = request.json['email']
+        user = db.admin.find_one({"email": email})
+        print(form.password.data)
+        print(user['password'])  
+        if user and check_password_hash(user['password'], form.password.data):
+            return jsonify({ "msg": "Welcome Back, " + json.loads(json_util.dumps(user["name"])) + "!"}), 200
+        else:
+            return jsonify({ "error": "Invalid email or password"}), 401
 
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
+    @app.route('/dashboard/')
+    @login_required
+    def dashboard():
+        return render_template('dashboard.html', name=current_user.name)
 
 
-@app.route('/protected')
-@jwt_required()
-def protected():
-    return '%s' % current_identity
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for('home'))
 
 
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-        'message': 'Resource Not Found: ' + request.url,
-        'status': 404
-    }
-    response = jsonify(message)
-    response.status_code = 404
-    return response
+    @app.route('/protected')
+    @jwt_required()
+    def protected():
+        return '%s' % current_identity
+
+
+    @app.errorhandler(404)
+    def not_found(error=None):
+        message = {
+            'message': 'Resource Not Found: ' + request.url,
+            'status': 404
+        }
+        response = jsonify(message)
+        response.status_code = 404
+        return response
 
 
 if __name__ == "__main__":
